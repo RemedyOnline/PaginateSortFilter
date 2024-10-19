284232bcc0fb7af06a074726acaf298d5d46e7e9
Michael Agyemang Prempeh <agyemangmichael555@gmail.com>
2024-10-19 14:48:59 +0000

    2:48pm - first push (Pausing to learn basics and backkk!)

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Button } from 'antd';
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Pagination... <br />
+        Sorting... <br />
+        Filtering...
+      </h1>
+      <Button type="primary">Button</Button>
+
+    </>
+  )
+}
+
+export default App
